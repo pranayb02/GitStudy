6839349f0e5737f679ac41e6016fa1056c8d99dd
PRANAY's GIT <pranaybadwaik02@gmail.com>
2023-02-07 10:28:59 +0530

    print ln

---
+++ src/Package1/ClassA.java
@@ -0,0 +1,20 @@
+package Package1;
+
+public class ClassA {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("MSD");
+	}
+    public void Profile() 
+    {
+    System.out.println("A well known cricketer");	
+    }
+    
+    public void AV() 
+    {
+    	System.out.println("A Bike collection");
+    	System.out.println("Car collection");
+    }
+    
+}
---
+++ src/Package1/ClassB.java
@@ -0,0 +1,22 @@
+package Package1;
+
+public class ClassB {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Pranay Badwaik");
+
+	}
+	
+	public void GeneralDiscussion()
+	{
+		System.out.println("Yet to create resume");
+	}
+	
+	public void AD() 
+	
+	{
+	System.out.println("Will look for experience.");	
+	}
+
+}
